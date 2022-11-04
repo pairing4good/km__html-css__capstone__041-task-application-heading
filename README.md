98b6552c9d00a2e3a5e16823f0eb9a2006963756
David Kessler <39646674+pairing4good@users.noreply.github.com>
2022-11-04 14:00:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 040 Form Width
+
+```
+Given that the user screen is wider than 625px 
+When the application is displayed 
+Then the task form displays a maximum width of 625px
+```
+
+```
+Given that the user screen is less than 625px 
+When the application is displayed 
+Then the task form displays according to the size of the screen
+```
+
+Steps:
+- the task-form-row class should have a maximum width of 625px
+
+Resources:
+- https://www.w3schools.com/cssref/pr_dim_max-width.asp
---
+++ test/verify.test.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/.." + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+describe('the task-form-row class', () => {
+  it('should have a maximum width of 625px', async () => {
+    const maxWidth = await page.$eval('.task-form-row', (row) => {
+      var style = window.getComputedStyle(row);
+      return style.getPropertyValue('max-width');
+    });
+      
+    expect(maxWidth).toBe('625px');
+  });
+});
